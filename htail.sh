ecc34610dba7c4c1cbba7eefe1e226da955621ab
Ryan Williams <ryan.rpwilliams@gmail.com>
2016-03-17 10:32:46 -0400

    Update htail.sh

--- htail.sh
+++ htail.sh
@@ -1,32 +1,72 @@
 #!/usr/bin/sh
 
+usage() {
+  echo "Usage: $0 -f [file] -n [offset]";
+  echo "";
+  echo "[file] : File to follow"
+  echo "[offset] : Range to highlight, rest will be formatted by default";
+  echo "";
+  exit 1
+}
+
 # Formatting variables.
 bold=$(tput bold)
 normal=$(tput sgr0)
-inputFile=$1
 
-# Syntax highlight tail logging.
-tail -f $inputFile | awk -v fileVar="$inputFile" '
-  BEGIN {
-    print "Following: " fileVar
-  }
+followFile() {
+	# Syntax highlight tail logging.
+	inputFile=$1
+	numOffset=$2
 
-  /INFO/ {
-  	print "\033[32m" $1, $2, $3, $4, $5 "\033[39m"
-  	printf "%s\t", ""
-  	for(i=6; i<=NF; i++) {printf "%s ", $i}
-  	print ""
-  }
-  /DEBUG/ {
-  	print "\033[36m" $1, $2, $3, $4, $5 "\033[39m"
-  	printf "%s\t", ""
-  	for(i=6; i<=NF; i++) {printf "%s ", $i}
-  	print ""
-  }
-  /ERROR/ {
-  	print "\033[31m" $1, $2, $3, $4, $5 "\033[39m"
-  	printf "%s\t", ""
-  	for(i=6; i<=NF; i++) {printf "%s ", $i}
-  	print ""
-  }
-'
+	tail -f $inputFile | awk -v fileVar="$inputFile" -v offset="$numOffset" '
+	  BEGIN {
+	    print "Following: " fileVar
+	    print "Offset: " offset
+	  }
+
+	  /INFO/ {
+	    for(i=1; i<=offset; i++) {printf "%s ", "\033[32m" $i "\033[39m"}
+	    for(j=offset+1; j<=NF; j++) {printf "%s ", $j}
+	    print ""
+	  }
+	  /DEBUG/ {
+	    for(i=1; i<=offset; i++) {printf "%s ", "\033[36m" $i "\033[39m"}
+	    for(j=offset+1; j<=NF; j++) {printf "%s ", $j}
+	    print ""
+	  }
+	  /ERROR/ {
+	    for(i=1; i<=offset; i++) {printf "%s ", "\033[31m" $i "\033[39m"}
+	    for(j=offset+1; j<=NF; j++) {printf "%s ", $j}
+	    print ""
+	  }
+	'
+}
+
+if [ "$#" -le 1 ]; then
+  usage
+fi
+
+while [ "$1" != "" ]; do
+  case $1 in
+  	  -h|--help)
+		usage
+		;;
+      -f) 
+        FILE=$2
+        shift
+        ;;  
+      -n) 
+        OFFSET=$2
+        shift
+        # Specify number of columns to highlight
+        # Makes script more verbose
+        ;;  
+      *)  
+        break
+        ;;  
+  esac
+  shift
+done
+
+# Run with given params.
+followFile $FILE $OFFSET
